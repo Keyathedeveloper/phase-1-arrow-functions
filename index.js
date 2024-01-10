91668affcd92cd4befc42b597c06a17c442bf1d1
TaKeya McFadden <takeyamcfadden@gmail.com>
2024-01-10 16:13:16 -0500

    lab complete

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = function(dividend, divisor) {
+    return  dividend / divisor;
+};
+
+const result = divide(2000, 100);
+console.log(result);
+
+const square = (num) => {
+    return num **2;
+};
+
+const add = (num1, num2) => {
+    return num1 + num2;
+}
